c8cccd21e32b16913ec0e2d0691959c783252858
Howdy07 <123291668+howdy07@users.noreply.github.com>
2023-05-20 11:43:34 +0530

    Update 4.rowtosparse.c

--- 4.rowtosparse.c
+++ 4.rowtosparse.c
@@ -1,29 +1,30 @@
-//convert rowmajor to sparse matrix
 #include<stdio.h>
-int main(){
-    int sparse[50][50],row,col,i,j,nzero,rowmajor[50][3];
-    printf("\nEnter the number of rows,cols and nzero elements\n");
-    scanf("%d %d %d",&row,&col,&nzero);
-
-    for(i=0;i<nzero;i++){
+void main(){
+    int sparse[50][50],rowmajor[50][3],i,j,row,sparserow,sparsecol,nzero;
+    printf("\nEnter the rows of the rowmajor matrix : ");
+    scanf("%d",&row);
+    printf("\nEnter the row major matrix \n");
+    for(i=0;i<row;i++){
         for(j=0;j<3;j++){
+            printf("\nEnter the value : ");
             scanf("%d",&rowmajor[i][j]);
         }
     }
-    for(i=0;i<row;i++){
-        for(j=0;j<col;j++){
+    sparserow=rowmajor[0][0];
+    sparsecol=rowmajor[0][1];
+    nzero=rowmajor[0][2];
+    for(i=0;i<sparserow;i++){
+        for(j=0;j<sparsecol;j++){
             sparse[i][j]=0;
         }
     }
-    for(i=1;i<nzero;i++){
-        sparse[rowmajor[i][0]][rowmajor[i][1]]= rowmajor[i][2];
+    for(i=0;i<nzero+1;i++){
+        sparse[rowmajor[i][0]][rowmajor[i][1]]=rowmajor[i][2];
     }
-    for(i=0;i<row;i++){
-        for(j=0;j<col;j++){
-            printf("%d ",sparse[i][j]);
+    for(i=0;i<sparserow;i++){
+        for(j=0;j<sparsecol;j++){
+            printf("%d  ",sparse[i][j]);
         }
         printf("\n");
     }
-
-    return 0;
 }
