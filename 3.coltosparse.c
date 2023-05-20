9ff55c44766391853edc08fa4aa505550feba4a0
Howdy07 <123291668+howdy07@users.noreply.github.com>
2023-05-20 13:31:20 +0530

    Update 3.coltosparse.c

--- 3.coltosparse.c
+++ 3.coltosparse.c
@@ -14,7 +14,7 @@ void main(){
         }
     }
     for(i=1;i<nzero;i++){
-        sparse[colmajor[0][i]][colmajor[1][i]]=colmajor[i][2];
+        sparse[colmajor[i][1]][colmajor[i][0]]=colmajor[i][2];
     }
     for(i=0;i<row;i++){
         for(j=0;j<col;j++){
